a3358a782ac5d9ef4348f30656fae6e6bafcd77e
carlLebrun <36409091+carlLebrun@users.noreply.github.com>
2021-04-15 11:42:07 +0200

    [LVL] Début d'un niveau de test [Feature] Ajout du chronomètre (pas encore terminé)

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 resetPos;
+    public Stopwatch stopwatch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            player.transform.SetPositionAndRotation(resetPos, player.transform.localRotation);
+            stopwatch.resetStopwatch();
+        }
+    }
+}
---
+++ Assets/Scripts/Stopwatch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Stopwatch : MonoBehaviour
+{
+    public float timeStart = 0;
+    public Text timeText;
+    public bool isPaused = false;
+
+    void Start()
+    {
+        timeText.text = timeStart.ToString();
+    }
+
+    void Update()
+    {
+        if (!isPaused)
+        {
+            timeStart += Time.deltaTime;
+            timeText.text = Math.Round(timeStart, 2).ToString();
+        }
+    }
+
+    public void resetStopwatch()
+    {
+        timeStart = 0;
+    }
+
+    public void setPause(bool ispaused)
+    {
+        isPaused = ispaused;
+    }
+}
